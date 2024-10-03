f4eb96c9ac0220060e7fdcafc1e9711513a89995
KARNATAKA SAIKUMAR <karnatakasaikumar1234@gmail.com>
2024-10-03 15:28:36 +0530

    second

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://rms-bakcend.onrender.com',
+  baseURL: 'https://rms-bakcend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
